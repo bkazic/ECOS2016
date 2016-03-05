7792b9ccceafc391e34353dca8edd8de23efc1d4
bkazic <blaz.kazic@ijs.si>
2016-03-05 21:30:05 +0100

    Initial commit of research for ECOS2016 conference.

---
+++ naive_model.py
@@ -0,0 +1,8 @@
+class NaiveModel:
+    
+    def __init__(self, target):
+        self.target = target
+        
+    def fit(self, X_train,y_train):
+        # No need to fit with naive models
+        pass    
